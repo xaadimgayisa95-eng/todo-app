f5f3ab4780557db9960edd3158e6305202c4766f
xaadimgayisa95-eng <xaadimgayisa95@gmail.com>
2026-07-15 07:33:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("script.js جاهز");
+function sayHello() {
+    alert("🎉 مبروك! أنت تتعلم البرمجة خطوة بخطوة مع ChatGPT.");
+}
+
+function updateTime() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString("ar");
+
+    document.getElementById("time").innerHTML =
+        "🕒 الوقت الآن: " + time;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
