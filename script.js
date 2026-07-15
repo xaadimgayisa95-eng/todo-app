4514a1df9307fdf7d08ff597f6715d333d2d1237
xaadimgayisa95-eng <xaadimgayisa95@gmail.com>
2026-07-15 08:00:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.getElementById("teacherBtn").addEventListener("click", function () {
-    alert("👨‍🏫 قريبًا ستتمكن من تسجيل نفسك كمعلم قرآن.");
-});
+
 
 document.getElementById("teacherBtn").addEventListener("click", function () {
     window.location.href = "teacher.html";
