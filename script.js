2e6f50faf0643986699ebce976c359588639d67f
xaadimgayisa95-eng <xaadimgayisa95@gmail.com>
2026-07-15 07:57:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("teacherBtn").addEventListener("click", function () {
     alert("👨‍🏫 قريبًا ستتمكن من تسجيل نفسك كمعلم قرآن.");
 });
 
-document.getElementById("studentBtn").addEventListener("click", function () {
-    alert("🎓 قريبًا ستتمكن من البحث عن معلم قرآن مناسب.");
+document.getElementById("teacherBtn").addEventListener("click", function () {
+    window.location.href = "teacher.html";
 });
 
 document.getElementById("aboutBtn").addEventListener("click", function () {
